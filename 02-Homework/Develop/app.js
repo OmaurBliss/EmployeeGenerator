6ae748d2b3e91b5a7e3a424d5f6df0e22b3f30a0
Omaur B <omaurbliss@gmail.com>
2021-03-12 15:38:50 -0600

    cleaned up manager render function

--- 02-Homework/Develop/app.js
+++ 02-Homework/Develop/app.js
@@ -38,25 +38,10 @@ function empRender() {
       }
     });
 }
-//render;
+
 
 function managerRender() {
-  //   const managerHTML = (answers) =>
-  //     `
-  //       <div class="card employee-card">
-  //       <div class="card-header">
-  //           <h2 class="card-title">${answers.name}</h2>
-  //           <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>{{ role }}</h3>
-  //       </div>
-  //       <div class="card-body">
-  //           <ul class="list-group">
-  //               <li class="list-group-item">ID: ${answers.id}</li>
-  //               <li class="list-group-item">Email: <a href="mailto:${answers.email}">${answers.email}</a></li>
-  //               <li class="list-group-item">Office number:${answers.officenumber}</li>
-  //           </ul>
-  //       </div>
-  //   </div>
-  //       `;
+
 
   inquirer
     .prompt([
